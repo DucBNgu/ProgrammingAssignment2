8527f3c8a929acf30ad5d99c7d218b428ccdd51c
Duc Nguyen <dbngu@ucdavis.edu>
2020-05-17 14:39:56 -0700

    Commit with both makeCacheMatrix and cacheSolve functions and comments about each function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Overall, this function allows you to set a cached matrix that's inverse can be
+## solved and cached for efficiency.
 
-## Write a short comment describing this function
+## This function makes a matrix with 4 behaviors that can retrieve and set the data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  +     inv <- NULL
+  +     set <- function(y) {
+    +         x <<- y
+    +         inv <<- NULL
+    +     }
+  +     get <- function() x
+  +     setInverse <- function() inv <<- solve(x)
+  +     getInverse <- function() inv
+  +     list(set = set, get = get,
+             +          setInverse = setInverse,
+             +          getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve fuction can retrieve the object set in makeCacheMatrix
+## it into the parent environment. "inv" is set to NULL as default.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  +     inv <- x$getInverse()
+  +     if(!is.null(inv)) {
+    +         message("getting cached data")
+    +         return(inv)
+    +     }
+  +     mat <- x$get()
+  +     inv <- solve(mat, ...)
+  +     x$setInverse()
+  +     inv
 }
